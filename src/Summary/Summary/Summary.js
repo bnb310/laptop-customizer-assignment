6542e96a9c4650dd786e3ea4592e7c77283e92eb
Nicki Pitcher <50596030+bnb310@users.noreply.github.com>
2019-09-05 09:16:23 -0600

    changing things back to original attempt

--- src/Summary/Summary/Summary.js
+++ src/Summary/Summary/Summary.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 
 class Summary extends Component {
     render () {
-        var summary = Object.keys(this.state.selected).map((feature, idx) => {
+        const summary = Object.keys(this.state.selected).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
             const selectedOption = this.state.selected[feature];
             return (
